9a501ed68ede3e12432429f0aad602fa4ad1dedb
De-Panther <depanther@gmail.com>
2018-01-25 23:02:37 +0200

    Game Scene

---
+++ Assets/Scripts/JointController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class JointController : MonoBehaviour {
+
+	public XRNode Joint;
+		
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = InputTracking.GetLocalPosition(Joint);
+		transform.localRotation = InputTracking.GetLocalRotation(Joint);
+	}
+}
