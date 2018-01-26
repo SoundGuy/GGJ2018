5dede4134455517f4e9e19e346ab7d07c3030576
DESKTOP-JVT02UE\admin <ggj2018ms@gmail.com>
2018-01-26 04:06:12 +0200

    lots of changes

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    void SelfDestructMe()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
