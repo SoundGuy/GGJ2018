bf6744addf8f1a0d8ec5500e7cb6032852767f0a
DESKTOP-JVT02UE\admin <ggj2018ms@gmail.com>
2018-01-26 22:49:04 +0200

    update riddle

---
+++ Assets/Scripts/UpdateRiddleText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateRiddleText : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void OnEnable() {
+        LevelController lvl = FindObjectOfType<LevelController>();
+        TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+        textmeshPro.text = lvl.ButtonsTexts[lvl.EndButton];
+	}
+	
+	
+}
