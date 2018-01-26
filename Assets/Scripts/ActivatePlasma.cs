3531da19793c887aac31590d1410b4cc5bfe5afb
DESKTOP-JVT02UE\admin <ggj2018ms@gmail.com>
2018-01-27 01:35:16 +0200

    added particles on hand

---
+++ Assets/Scripts/ActivatePlasma.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePlasma : MonoBehaviour {
+
+    public string InputAxisName;
+
+    public GameObject particles;
+
+    // Use this for initialization
+    void Start () {
+   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       if (Input.GetButton(InputAxisName))
+        {
+            particles.SetActive(true);
+        }
+        else
+        {
+            particles.SetActive(false);
+        }
+
+    }
+}
