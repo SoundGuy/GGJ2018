cf88ce826d02fc13e4f1f3a38f1cc94ae20ec368
DESKTOP-JVT02UE\admin <ggj2018ms@gmail.com>
2018-01-26 19:13:43 +0200

    name generator

---
+++ Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameGenerator : MonoBehaviour {
+
+    public List<string> prefixes;
+    public List<string> syllables;
+    public List<string> suffixes;
+    public int percengateOfThreesyllableWord = 2;
+
+    List<string> getSimilarWords(int num)
+    {
+
+
+        int pref = UnityEngine.Random.Range(0, prefixes.Count);
+        int suff = UnityEngine.Random.Range(0, suffixes.Count);
+        List<string> retVal = new List<string>();
+
+        for (int i=0;i<num;i++) {
+            int newPrefixNum = (i%2==0)? pref : ((pref + i) % prefixes.Count);
+            int newSuufiexNum = (i % 2 != 0) ? suff : ((suff + i) % suffixes.Count);
+            string suffix = prefixes[newPrefixNum];
+            string prefix = suffixes[newSuufiexNum];
+            string name = string.Format("{0}{1}", prefix, suffix);
+        retVal.Add(name);
+        }
+
+        return retVal;
+    }
+    string getWord(int midLength)
+    {
+        string midPart = "";
+        for (int i=0;i<midLength;i++ )
+        {
+            midPart = string.Format("{0}{1}", midPart, GetRandomItem(syllables));
+        }
+        return string.Format("{0}{1}{2}", GetRandomItem(prefixes), midPart, GetRandomItem(suffixes));
+    }
+
+    private object GetRandomItem(List<string> inList)
+    {
+        int i = UnityEngine.Random.Range(0, inList.Count);
+        return (inList[i]);
+    }
+
+
+    string getRandomWord() {
+        if (UnityEngine.Random.Range(0,10) <2)
+        {
+            return getWord(1);
+        }
+        return getWord(0);      
+}
+    // Use this for initialization
+    void Start () {
+		for (int i=0;i<50;i++ )
+        {
+            Debug.Log(getRandomWord());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
