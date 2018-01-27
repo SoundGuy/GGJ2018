eeb37e038e25a57f30124f4a9c795a1fc6a39acc
DESKTOP-JVT02UE\admin <ggj2018ms@gmail.com>
2018-01-27 04:27:04 +0200

    added start scene

---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateText : MonoBehaviour {
+
+    public string TextToChangeTo;
+    // Use this for initialization
+    public void UpdateTheText()
+    {
+    
+        TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+        textmeshPro.text = TextToChangeTo;
+    }
+
+
+    public void UpdateTheText(string inText)
+    {
+        TextToChangeTo = inText;
+        UpdateTheText();
+    }
+}
